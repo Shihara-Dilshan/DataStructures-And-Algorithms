e68ab17e809e7d59cd784297ff07f016a2e41ae3
root <root@kali.noobmaster>
2020-06-19 16:56:14 -0400

    python

---
+++ stacks/Python/stacks.py
@@ -0,0 +1,22 @@
+# implementing stacks using python
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.nextNode = None
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_next_node(self):
+        return self.nextNode
+
+    def set_next_node(self, next_node):
+        self.nextNode = next_node
+
+
