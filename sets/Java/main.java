eb4b50ee5bdec832c24e31381d57c5730233b19a
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-06-21 12:04:46 -0400

    sets implemeting inhava with declarative approach

--- sets/Java/main.java
+++ sets/Java/main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 interface setsAbstract{
 
@@ -8,10 +9,10 @@ interface setsAbstract{
     boolean add(int value);
     boolean remove(int value);
     int size();
-    List<Integer> union(sets set);
-    List<Integer> intersection(sets set);
-    List<Integer> difference(sets set);
-    boolean subset(sets set);
+    List<Integer> union(sets AnotherSet);
+    List<Integer> intersection(sets AnotherSet);
+    List<Integer> difference(sets AnotherSet);
+    boolean subset(sets AnotherSet);
 
 }
 
@@ -27,50 +28,63 @@ class sets implements setsAbstract{
 
     @Override
     public boolean has(int value) {
-        set.stream()
-
-
-
-
+        boolean result = this.set.stream()
+                .anyMatch( element -> Integer.valueOf(element).equals(value));
+        return result;
     }
 
     @Override
     public List<Integer> values() {
-        return null;
+        return set;
     }
 
     @Override
     public boolean add(int value) {
+        if(!this.has(value)){
+            this.set.add(value);
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean remove(int value) {
+        if(this.has(value)){
+            this.set.remove(value);
+            return true;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.set.size();
     }
 
     @Override
-    public List<Integer> union(sets set) {
-        return null;
+    public List<Integer> union(sets AnotherSet) {
+        AnotherSet.values().stream().forEach(value -> this.set.add(value));
+        return set;
     }
 
     @Override
-    public List<Integer> intersection(sets set) {
-        return null;
+    public List<Integer> intersection(sets AnotherSet) {
+        List<Integer> intersection = AnotherSet.values().stream()
+                .filter(value -> this.has(value))
+                .collect(Collectors.toList());
+        return intersection;
     }
 
     @Override
-    public List<Integer> difference(sets set) {
-        return null;
+    public List<Integer> difference(sets AnotherSet) {
+        List<Integer> difference = AnotherSet.values().stream()
+                .filter(value -> !this.has(value))
+                .collect(Collectors.toList());
+        return difference;
     }
 
     @Override
-    public boolean subset(sets set) {
+    public boolean subset(sets AnotherSet) {
         return false;
     }
 }
@@ -78,6 +92,17 @@ class sets implements setsAbstract{
 
 public class main {
     public static void main(String[] args) {
+        sets set1 = new sets();
+        sets set2 = new sets();
+
+        set1.add(12);
+        set1.add(1222);
+        set1.add(12292);
+        set2.add(12);
+        set2.add(92);
+        set2.add(12292);
+
+        System.out.println(set1.difference(set2));
 
     }
 }
