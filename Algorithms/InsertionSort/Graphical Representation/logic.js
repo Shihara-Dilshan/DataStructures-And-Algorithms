e524ddfbc5c9634590dc39294b2dc4c11406cf1d
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-03-12 06:29:28 +0800

    UI improved

--- Algorithms/InsertionSort/Graphical Representation/logic.js
+++ Algorithms/InsertionSort/Graphical Representation/logic.js
@@ -1,11 +1,18 @@
 "use strict";
 
 (function(){
-   const values = [15,17,11,12,42,63,75,82,18,16,19,15,57,16,20,31,15,22,38,36,45,1,94,7,0,10,20,14];//array to represent
+   const values = [];//array to represent
    const oneBar = document.querySelectorAll('.block');//each bar in the gragh(GUI)
    const upperArray = document.querySelectorAll('.Ablock');//top array items(GUI)
    const startBtn = document.getElementById("start");//start button
    const restartBtn = document.getElementById("resstart");//restart button
+   
+   
+   //fill array with random values between 0 and 100
+   for(let i=0; i<35; i++){
+       let randomNumber = Math.ceil(Math.random()*100); 
+       values.push(randomNumber);
+   }
    
    /*
      set bar GUI initial values
@@ -13,7 +20,7 @@
    oneBar.forEach( (bar, index) => {
       let per = values[index]; 
       setTimeout( () => {
-         bar.style.backgroundImage = `-webkit-linear-gradient(bottom, #ffd600, #ffd600 ${per}%, transparent 0%, transparent 100%)`;
+         bar.style.backgroundImage = `-webkit-linear-gradient(bottom, #ffd600, red ${per}%, transparent 0%, transparent 100%)`;
       }, per);
    });
    
@@ -56,8 +63,11 @@
             inputArr[j+1] = current;
             await handleBarAnimation(inputArr);
             await handleUpperArray(inputArr);
-            upperArray[0].style.border = "4px solid green";
-            upperArray[i].style.border = "4px solid green";
+            upperArray[j+1].style.border = "4px solid red";
+            await delay();
+            upperArray[j+1].style.border = "4px solid #388e3c";
+            upperArray[0].style.border = "4px solid #388e3c";
+            upperArray[i].style.border = "4px solid #388e3c";
             
         }
    }
@@ -71,7 +81,7 @@
            oneBar.forEach( (bar, index) => {
              let per = inputArray[index]; 
              setTimeout( () => {
-                bar.style.backgroundImage = `-webkit-linear-gradient(bottom, #ffd600, #ffd600 ${per}%, transparent 0%, transparent 100%)`;
+                bar.style.backgroundImage = `-webkit-linear-gradient(bottom, #ffd600, red ${per}%, transparent 0%, transparent 100%)`;
                 resolve("ok");
              } , 500);
           });
@@ -89,6 +99,17 @@
                 });
                 resolve("ok");
              } , 100);
+       });
+   }
+   
+   /*
+   promise make a delay
+   */
+   function delay(){
+       return new Promise( (resolve, reject) => {
+             setTimeout( () => {
+                resolve("ok");
+             } , 500);
        });
    }
    
